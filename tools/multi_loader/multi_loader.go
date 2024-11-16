f6b354737659f20e0d687396af67aa6427411b45
Lenson <nosnelmil@gmail.com>
2024-11-16 14:35:24 +0800

    add multi loader runner execution
    Signed-off-by: Lenson <nosnelmil@gmail.com>
    
    update default multi loader config path
    
    Signed-off-by: Lenson <nosnelmil@gmail.com>

--- tools/multi_loader/multi_loader.go
+++ tools/multi_loader/multi_loader.go
@@ -6,10 +6,14 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/vhive-serverless/loader/tools/multi_loader/runner"
 )
 
 var (
+	multiLoaderConfigPath    = flag.String("multiLoaderConfig", "tools/multi_loader/multi_loader_config.json", "Path to multi loader configuration file")
     verbosity     = flag.String("verbosity", "info", "Logging verbosity - choose from [info, debug, trace]")
+	iatGeneration = flag.Bool("iatGeneration", false, "Generate iats only and skip invocations")
+	generated     = flag.Bool("generated", false, "If iats were already generated")
 )
 
 func init() {
@@ -35,5 +39,24 @@ func initLogger() {
 }
 
 func main() {
-	// Run multi loader
+	log.Info("Starting multiloader")
+	// Create multi loader driver
+	multiLoaderDriver, err := runner.NewMultiLoaderRunner(*multiLoaderConfigPath, *verbosity, *iatGeneration, *generated)
+	if err != nil {
+		log.Fatalf("Failed to create multi loader driver: %v", err)
+	}
+	// Dry run
+	multiLoaderDriver.RunDryRun()
+
+	// Check if dry run was successful
+	if !multiLoaderDriver.DryRunSuccess {
+		log.Fatal("Dry run failed. Exiting...")
+	}
+
+	// Actual run
+	log.Info("Running experiments")
+	multiLoaderDriver.RunActual()
+
+	// Finish
+	log.Info("All experiments completed")
 }
