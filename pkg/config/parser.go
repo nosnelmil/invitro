482db6c8aebd73276045f62d5e4a26cb57e55bfb
Lenson <nosnelmil@gmail.com>
2024-11-16 13:21:48 +0800

    refactor multi loader config
    Signed-off-by: Lenson <nosnelmil@gmail.com>

--- pkg/config/parser.go
+++ pkg/config/parser.go
@@ -29,6 +29,7 @@ import (
 	"os"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/vhive-serverless/loader/pkg/common"
 )
 
 type FailureConfiguration struct {
@@ -82,38 +83,6 @@ type LoaderConfiguration struct {
 	DAGMode                      bool `json:"DAGMode"`
 }
 
-type MutliLoaderConfiguration struct {
-	Experiments 	[]LoaderExperiment 	`json:"Experiments"`
-	BaseConfigPath  string             	`json:"BaseConfigPath"`
-	// Optional
-	PreScript 		string 				`json:"PreScript"`
-	PostScript	 	string 				`json:"PostScript"`
-	MasterNode 		string 				`json:"MasterNode"`
-	AutoScalerNode 	string 				`json:"AutoScalerNode"`
-	ActivatorNode 	string 				`json:"ActivatorNode"`
-	LoaderNode 		string 				`json:"LoaderNode"`
-	WorkerNodes 	[]string 			`json:"WorkerNodes"`
-	Metrics         []string			`json:"Metrics"`
-}
-
-type LoaderExperiment struct {
-	Name    		string           		`json:"Name"`
-	Config  		map[string]interface{}	`json:"Config"`
-	// A combination of format and values or just dir should be specified
-	TracesDir 		string 					`json:"TracesDir"`
-
-	TracesFormat 	string 					`json:"TracesFormat"`
-	TraceValues 	[]interface{}			`json:"TraceValues"`
-
-	// Optional
-	OutputDir 		string 					`json:"OutputDir"`
-	Verbosity		string 			   		`json:"Verbosity"`
-	IatGeneration 	bool 					`json:"IatGeneration"`
-	Generated 		bool 					`json:"Generated"`
-	PreScript	 	string 					`json:"PreScript"`
-	PostScript	 	string 					`json:"PostScript"`
-}
-
 func ReadConfigurationFile(path string) LoaderConfiguration {
 	byteValue, err := os.ReadFile(path)
 	if err != nil {
@@ -147,13 +116,13 @@ func ReadFailureConfiguration(path string) *FailureConfiguration {
 	return &config
 }
 
-func ReadMultiLoaderConfigurationFile(path string) MutliLoaderConfiguration {
+func ReadMultiLoaderConfigurationFile(path string) common.MutliLoaderConfiguration {
 	byteValue, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var config MutliLoaderConfiguration
+	var config common.MutliLoaderConfiguration
 	err = json.Unmarshal(byteValue, &config)
 	if err != nil {
 		log.Fatal(err)
